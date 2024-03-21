2c7d9cca02d2e25e013e02d4633be6ed9b49361d
Disha <147893301+disha-a-g@users.noreply.github.com>
2024-03-21 18:55:12 -0500

    Update emotion_detection.py

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -1,1 +1,15 @@
+import requests  # You'll likely need this library
+
+def emotion_detector(text_to_analyze):
+   url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+   headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+   payload = {"raw_document": {"text": text_to_analyze}}
+
+   response = requests.post(url, headers=headers, json=payload)
+
+   if response.status_code == 200:
+       return response.json()['text']  # Adjust the path within the response if needed
+   else:
+       return None  # Or handle the error differently
+
 
